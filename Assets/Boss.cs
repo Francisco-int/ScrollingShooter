928955e362bd4d42f76e874ad8ffb92ff77d1a9d
Francisco-int <franciscos1205@gmail.com>
2024-05-25 00:19:48 -0300

    0.5

---
+++ Assets/Boss.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+
+   [SerializeField] int vida;
+   [SerializeField] Transform cañonUno;
+   [SerializeField] Transform cañonDos;
+    [SerializeField] GameObject proyectil;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Disparo()
+    {
+
+    }
+
+
+}
--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -8,13 +8,18 @@ public class Enemy : MonoBehaviour
     [SerializeField] float velocidad;
     [SerializeField] public int numEnemy;
     [SerializeField] GameManager gameManager;
+    [SerializeField] Transform player;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     private void Awake()
     {
+        player = GameObject.Find("Player").GetComponent<Transform>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         numEnemy = gameManager.enemynum;
     }
@@ -22,11 +27,13 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(transform.forward * Time.deltaTime * velocidad);
+        transform.position = transform.position + new Vector3(0,0,velocidad * Time.deltaTime);
         if(transform.position.z > 15 || transform.position.z > 7)
         {
             Dead();
         }
+        transform.LookAt(player.transform.position);
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -35,6 +42,7 @@ public class Enemy : MonoBehaviour
             Dead();
         }
     }
+   
     
     void Dead()
     {
